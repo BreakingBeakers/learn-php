f7547bfe49529b1c0088527fc8556d274b8f378c
bbrickner <bbrickner@wastequip.com>
2025-04-03 15:48:24 -0400

    Finish Dynamic Web Application Section

---
+++ dynamic_web_applications/views/404.php
@@ -0,0 +1,13 @@
+<?php require('partials/head.php'); ?>
+<?php require('partials/nav.php') ?>
+
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <div class="text-xl mb-4">
+            Sorry, page is not found.
+        </div>
+        <a href="/" class="pl-3 underline">Go Back Home.</a>
+    </div>
+</main>
+
+<?php require('partials/footer.php'); ?>
